28000ba6c47c647af3d9c0349af145e974124b38
magurh <magureanuhoria@gmail.com>
2025-04-23 14:50:49 +0100

    fix(centralized gossip): replace legacy np RNG calls

--- src/centralized_gossip/protocols.py
+++ src/centralized_gossip/protocols.py
@@ -10,38 +10,25 @@ def slush(
     num_param_nodes: int,
 ) -> tuple[int, int]:
     """
-    Centralized Slush protocol implementation
-
-    :param num_nodes: number of nodes
-    :param num_byz_nodes: number of perfectly Byzantine nodes
-    :param colors: 1d array of 0s and 1s
-    :param k_slush: Slush protocol parameter
-    :param alpha_slush: Slush protocol parameter
-    :param num_param_nodes: run algorithm until all but param nodes agree on a state
-
-    :return: output of network (0 or 1), number of rounds
+    Centralized Slush protocol implementation.
     """
+    rng = np.random.default_rng()
     num_rounds = 0
     sum_colors = np.sum(colors)
 
     while True:
         if num_param_nodes > sum_colors or sum_colors > num_nodes - num_param_nodes:
-            # stop when all but num_param_nodes agree on a state
             break
 
-        # Pick a random node that is NOT Byzantine: index >= byz
-        random_non_byz_node = np.random.randint(num_byz_nodes, num_nodes)
+        # Pick a random node that is NOT Byzantine
+        random_non_byz_node = rng.integers(num_byz_nodes, num_nodes)
 
         # Pick a random sample of size k
-        random_sample_decision = np.sum(
-            colors[np.random.choice(num_nodes, size=k_slush, replace=False)]
-        )
+        random_sample = rng.choice(num_nodes, size=k_slush, replace=False)
+        random_sample_decision = np.sum(colors[random_sample])
 
-        # Compare with threshold parameter
         if random_sample_decision >= alpha_slush:
-            # Readjust sum_colors so we don't have to compute from scratch
             sum_colors = sum_colors - colors[random_non_byz_node] + 1
-            # Update node color
             colors[random_non_byz_node] = 1
         elif k_slush - random_sample_decision >= alpha_slush:
             sum_colors -= colors[random_non_byz_node]
@@ -49,8 +36,8 @@ def slush(
 
         num_rounds += 1
 
-    random_sample_decision = 1 if sum_colors > num_nodes // 2 else 0
-    return random_sample_decision, num_rounds
+    final_decision = 1 if sum_colors > num_nodes // 2 else 0
+    return final_decision, num_rounds
 
 
 def weighted_slush(
@@ -58,24 +45,14 @@ def weighted_slush(
     num_byz_nodes: int,
     colors: np.ndarray,
     k_slush: int,
-    alpha_slush: np.ndarray,
+    alpha_slush: float,
     num_param_nodes: int,
     weight_matrix: np.ndarray,
 ) -> tuple[int, int]:
     """
-    Global Slush protocol implementation using weight matrix for local aggregation
-
-    :param num_nodes: number of nodes
-    :param num_byz_nodes: number of perfectly Byzantine nodes
-    :param colors: 1d array of 0s and 1s
-    :param k_slush: Slush protocol parameter
-    :param alpha_slush: Slush protocol parameter
-    :param num_param_nodes: run algorithm until all but param nodes agree on a state
-    :param weight_matrix: a (num_nodes) array containing weights global w_j to be used
-        in local aggregations
-
-    :return: output of network (0 or 1) and number of rounds taken
+    Global Slush protocol implementation using weight matrix for local aggregation.
     """
+    rng = np.random.default_rng()
     num_rounds = 0
     sum_colors = np.sum(colors)
 
@@ -83,32 +60,21 @@ def weighted_slush(
         if num_param_nodes > sum_colors or sum_colors > num_nodes - num_param_nodes:
             break
 
-        # Pick a random node that is NOT Byzantine: index >= byz
-        random_non_byz_node = np.random.randint(num_byz_nodes, num_nodes)
+        random_non_byz_node = rng.integers(num_byz_nodes, num_nodes)
+        random_sample = rng.choice(num_nodes, size=k_slush, replace=False)
 
-        # Pick a random sample of size k
-        random_sample = np.random.choice(num_nodes, size=k_slush, replace=False)
-
-        # Compute decision of the sample and rescale
-        random_sample_decision = (
-            k_slush
-            * np.dot(colors[random_sample], weight_matrix[random_sample])
-            / np.sum(weight_matrix[random_sample])
-        )
+        weights = weight_matrix[random_sample]
+        weighted_sum = np.dot(colors[random_sample], weights)
+        random_sample_decision = (k_slush * weighted_sum) / np.sum(weights)
 
         if random_sample_decision >= alpha_slush:
-            # Readjust sum_colors so we don't have to compute from scratch
             sum_colors = sum_colors - colors[random_non_byz_node] + 1
-            # Update node color
             colors[random_non_byz_node] = 1
         elif k_slush - random_sample_decision >= alpha_slush:
-            # Readjust sum_colors so we don't have to compute from scratch
             sum_colors -= colors[random_non_byz_node]
-            # Update node color
             colors[random_non_byz_node] = 0
 
         num_rounds += 1
 
-    random_sample_decision = 1 if sum_colors > num_nodes // 2 else 0
-
-    return random_sample_decision, num_rounds
+    final_decision = 1 if sum_colors > num_nodes // 2 else 0
+    return final_decision, num_rounds
